cbbf3f262a3e49ab465ca0b98f833b1f4389be53
cob <unitimes@naver.com>
2014-05-02 02:34:43 +0900

    week7

--- week7/Lab4.cs
+++ week7/Lab4.cs
@@ -2,37 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-class Lab3 {
-    public static void Main() {
-        List <int> ll = new List<int>();
-        string t1; 
-        int idx1;
-        int idx2;
-        int ent1;
-        int ent2;
-
-        for(int i = 1; i <= 10; i++) {
-            ll.Add(i);
-        }   
-        foreach(int i in ll) 
-            Console.Write("{0} ", i); 
-        Console.WriteLine();
-        while(true) {
-            t1 = Console.ReadLine();
-            if(t1 == "-999")
-                break;
-            string[] t2 = t1.Split(' ');
-            ent1 = Convert.ToInt32(t2[0]);
-            ent2 = Convert.ToInt32(t2[1]);
-            idx1 = ll.IndexOf(ent1);
-            idx2 = ll.IndexOf(ent2);
-            ll.RemoveAt(idx1);
-            ll.Insert(idx1, ent2);
-            ll.RemoveAt(idx2);
-            ll.Insert(idx2, ent1);
-            foreach(int i in ll) 
-                Console.Write("{0} ", i); 
+class RunLotto
+{
+    static void Main(string[] args)
+    {
+        List<int> l = new List<int>();
+        for (int i = 1; i <= 45; i++)
+            l.Add(i);
+        for (int j = 0; j < 50; j++)
+        {
+            Lotto.Shuffle(ref l);
+            for (int i = 0; i < 6; i++)
+                Console.Write("{0} ", l[i]);
             Console.WriteLine();
-        }   
-    }   
+        }
+    }
+}
+
+class Lotto
+{
+    public static void Shuffle(ref List<int> l)
+    {
+        int temp1;
+        int temp2;
+        int idx;
+        Random r = new Random();
+        for (int i = 0; i < l.Count; i++)
+        {
+            idx = r.Next(0, (l.Count - 1));
+            temp1 = l[i];
+            temp2 = l[idx];
+            l.RemoveAt(i);
+            l.Insert(i, temp2);
+            l.RemoveAt(idx);
+            l.Insert(idx, temp1);
+        }
+    }
 }
---
+++ week7/Wallet.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class RunWallet
+{
+    static void Main(string[] args)
+    {
+        List<Student> s = new List<Student> ();
+        string tName;
+        int num;
+
+        Console.WriteLine("학생 이름을 입력하세요. (qqq: 종료)");
+        while(true)
+        {
+            tName = Console.ReadLine();
+            if (tName == "qqq")
+                break;
+            s.Add(new Student(tName));
+        }
+
+        Console.Write("몇명 선택하시겠습니까? (1 - {0})", Student.total);
+        num = Convert.ToInt32(Console.ReadLine());
+        Wallet.Shuffle(ref s);
+
+        for (int i = 0; i < num; i++)
+        {
+            if (i < (num - 1))
+                Console.Write(s[i].name + ", ");
+            else
+                Console.WriteLine(s[i].name + " 님이 밥을 사세요.");
+        }
+    }
+}
+
+public class Student
+{
+    public static int total;
+    public string name;
+    public int id;
+
+    public Student(string name)
+    {
+        total++;
+        this.name = name;
+        id = total;
+    }
+}
+
+public class Wallet
+{
+    public static void Shuffle(ref List<Student> s)
+    {
+        Student temp1;
+        Student temp2;
+        int idx;
+        Random r = new Random();
+        for (int i = 0; i < s.Count; i++)
+        {
+            idx = r.Next(0, (s.Count - 1));
+            temp1 = s[i];
+            temp2 = s[idx];
+            s.RemoveAt(i);
+            s.Insert(i, temp2);
+            s.RemoveAt(idx);
+            s.Insert(idx, temp1);
+        }
+    }
+}
